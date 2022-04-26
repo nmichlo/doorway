db878c1f2675589b8b31faee9ef882b0c5b53a35
Nathan <NathanJMichlo@gmail.com>
2022-04-27 01:24:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+
+<p align="center">
+    <h1 align="center">🧪 REPLACE</h1>
+    <p align="center">
+        <i>replace this description!</i>
+    </p>
+</p>
+
+<p align="center">
+    <a href="https://choosealicense.com/licenses/mit/" target="_blank">
+        <img alt="license" src="https://img.shields.io/github/license/nmichlo/REPLACE?style=flat-square&color=lightgrey"/>
+    </a>
+    <a href="https://pypi.org/project/REPLACE" target="_blank">
+        <img alt="python versions" src="https://img.shields.io/pypi/pyversions/REPLACE?style=flat-square"/>
+    </a>
+    <a href="https://pypi.org/project/REPLACE" target="_blank">
+        <img alt="pypi version" src="https://img.shields.io/pypi/v/REPLACE?style=flat-square&color=blue"/>
+    </a>
+    <a href="https://github.com/nmichlo/REPLACE/actions?query=workflow%3Atest">
+        <img alt="tests status" src="https://img.shields.io/github/workflow/status/nmichlo/REPLACE/test?label=tests&style=flat-square"/>
+    </a>
+    <a href="https://codecov.io/gh/nmichlo/REPLACE/">
+        <img alt="code coverage" src="https://img.shields.io/codecov/c/gh/nmichlo/REPLACE?token=86IZK3J038&style=flat-square"/>
+    </a>
+</p>
+
+<p align="center">
+    <p align="center">
+        <a href="https://github.com/nmichlo/REPLACE/issues/new/choose">Contributions</a> are welcome!
+    </p>
+</p>
+
+----------------------
+
+## Table Of Contents
+
+- [Overview](#overview)
+
+----------------------
+
+## Overview
+
+This project is a template that includes docs, pypi publishing and automated testing with github actions.
+- Find the entries in this repo that need replacing/deletion with `grep -rni ./ -e "replace"`
+
+Get started with `REPLACE` by installing it with $`pip install REPLACE` or cloning this repository.
---
+++ setup.py
@@ -0,0 +1,51 @@
+
+import setuptools
+
+
+# ========================================================================= #
+# HELPER                                                                    #
+# ========================================================================= #
+
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+with open('requirements.txt', 'r') as f:
+    install_requires = (req[0] for req in map(lambda x: x.split('#'), f.readlines()))
+    install_requires = [req for req in map(str.strip, install_requires) if req]
+
+
+# ========================================================================= #
+# SETUP                                                                     #
+# ========================================================================= #
+
+
+setuptools.setup(
+    name="REPLACE",
+    author="Nathan Juraj Michlo",
+    author_email="NathanJMichlo@gmail.com",
+
+    version="0.0.1.dev1",
+    python_requires=">=3.8",
+    packages=setuptools.find_packages(),
+
+    install_requires=install_requires,
+
+    url="https://github.com/nmichlo/REPLACE",
+    description="replace this description!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
+
+
+# ========================================================================= #
+# END                                                                       #
+# ========================================================================= #
