85a25db016a63e545c67a31bc92125f8a3caa964
Nathan Michlo <NathanJMichlo@gmail.com>
2022-05-27 03:35:48 +0200

    better sharding

--- doorway/_shard.py
+++ doorway/_shard.py
@@ -1,12 +1,15 @@
 import os
 from pathlib import Path
 from typing import Callable
+from typing import Iterable
 from typing import List
 from typing import Optional
 from typing import Sequence
 from typing import Tuple
 from typing import Union
+from typing import TypeVar
 
+from doorway._hash import Hash
 from doorway._hash import HashAlgo
 from doorway._hash import hash_str
 
@@ -16,44 +19,51 @@ from doorway._hash import hash_str
 # ========================================================================= #
 
 
+T = TypeVar('T')
+
 _SHARD_KEYS = {
     'basename': os.path.basename,
     'abspath': os.path.abspath,
     'input': lambda x: x,
-    None: lambda x: x,
 }
 
-ShardKey = Union[str, None, Callable[[str], str]]
+ShardKey = Optional[Union[str, Callable[[T], str]]]
 
 
 def shard_hash(
-    file_path: Union[str, Path],
-    shard_key: ShardKey = 'basename',
+    value: T,
+    shard_key: ShardKey[T] = None,
     hash_algo: Optional[HashAlgo] = None,
-):
-    assert isinstance(file_path, (str, Path)), f'file_path must be a str or Path, got type: {type(file_path)} with value: {repr(file_path)}'
-    file_path = str(file_path)
+) -> Hash:
     # get the hash data function
-    if shard_key in _SHARD_KEYS:
-        shard_key = _SHARD_KEYS[shard_key]
-    elif not callable(shard_key):
-        raise ValueError(f'shard_key must be callable or one of: {list(_SHARD_KEYS.keys())}, got: {repr(shard_key)}')
+    if shard_key is None:
+        pass
+    elif callable(shard_key):
+        value = shard_key(value)
+    elif isinstance(shard_key, str):
+        fn = _SHARD_KEYS.get(shard_key, None)  # cannot be None here
+        if fn is None:
+            raise KeyError(f'if shard_key is a str, it must be one of: {list(_SHARD_KEYS.keys())}, got: {repr(shard_key)}')
+        value = fn(value)
+    else:
+        raise ValueError(f'shard_key must be a str, callable or None, got: {repr(shard_key)}')
+    # get the string
+    assert isinstance(value, (str, Path)), f'The value after shard_key is applied must be a str or Path, instead got type: {type(value)}, with value: {repr(value)}'
     # compute the hash
-    return hash_str(shard_key(file_path), hash_algo=hash_algo)
+    return hash_str(str(value), hash_algo=hash_algo)
 
 
 def shard_idx(
-    file_path: Union[str, Path],
+    value: T,
     num_shards: int,
-    shard_key: ShardKey = 'basename',
+    shard_key: ShardKey[T] = None,
     hash_algo: Optional[HashAlgo] = None,
-):
+) -> int:
     assert isinstance(num_shards, int) and (num_shards > 0), f'num_shards must be an integer that is > 0, got: {repr(num_shards)}'
-    # compute the hash for the file
-    hash = shard_hash(file_path, shard_key=shard_key, hash_algo=hash_algo)
+    # compute the hash for the value
+    hash = shard_hash(value, shard_key=shard_key, hash_algo=hash_algo)
     # convert hashes to integers, and assign to correct split
-    idx = int(hash, 16) % num_shards
-    return idx
+    return int(hash, 16) % num_shards
 
 
 # ========================================================================= #
@@ -62,19 +72,19 @@ def shard_idx(
 
 
 _SHARD_RETURNS = {
-    'pairs': lambda i, file_path: (i, file_path),
-    'indices': lambda i, file_path: i,
-    'values': lambda i, file_path: file_path,
+    'pairs':   lambda i, value: (i, value),
+    'indices': lambda i, value: i,
+    'values':  lambda i, value: value,
 }
 
 
 def sharded(
-    file_paths: Sequence[Union[str, Path]],
+    values: Iterable[T],
     num_shards: int,
-    shard_key: ShardKey = 'basename',
+    shard_key: ShardKey[T] = None,
     hash_algo: Optional[HashAlgo] = None,
     returns: str = 'values'
-) -> Union[List[List[int]], List[List[str]], List[List[Tuple[int, str]]]]:
+) -> list:
     """
     Shard files based on their hashes instead of random seeds
     """
@@ -85,31 +95,31 @@ def sharded(
     # create new array of shards
     shards = [[] for _ in range(num_shards)]
     # assign paths to shards
-    for i, file_path in enumerate(file_paths):
-        idx = shard_idx(file_path, num_shards, shard_key=shard_key, hash_algo=hash_algo)
-        shards[idx].append(value_getter(i, file_path))
+    for i, value in enumerate(values):
+        idx = shard_idx(value, num_shards, shard_key=shard_key, hash_algo=hash_algo)
+        shards[idx].append(value_getter(i, value))
     # results
     return shards
 
 
 def sharded_and_grouped(
-    file_paths: Sequence[str],
-    group_sizes: Sequence[int],
-    shard_key: ShardKey = 'basename',
+    values: Iterable[T],
+    group_sizes: Iterable[int],
+    shard_key: ShardKey[T] = None,
     hash_algo: Optional[HashAlgo] = None,
     returns: str = 'values'
-) -> Union[List[List[int]], List[List[str]], List[List[Tuple[int, str]]]]:
+) -> list:
     """
     Shard files based on their hashes instead of random seeds
     -- This is useful if you need to split a dataset, but you expect changes to be made to it,
        eg. files will always randomly be assigned to the same shared
     """
-
+    group_sizes = list(group_sizes)
     # checks
     assert all(isinstance(size, int) and (size >= 0) for size in group_sizes), f'values of group_sizes must be integers that are >= 0, got: {repr(group_sizes)}'
     # get all the shards
     shards = sharded(
-        file_paths=file_paths,
+        values=values,
         num_shards=sum(group_sizes),
         shard_key=shard_key,
         hash_algo=hash_algo,
