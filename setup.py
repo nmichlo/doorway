9857204ac3e92a74f209f5f8d0f4ad54785d74d3
Nathan Michlo <NathanJMichlo@gmail.com>
2022-08-17 03:01:50 +0200

    version bump v0.5.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
     author="Nathan Juraj Michlo",
     author_email="NathanJMichlo@gmail.com",
 
-    version="0.4.0",
+    version="0.5.0",
     python_requires=">=3.8",
     packages=setuptools.find_packages(),
 
