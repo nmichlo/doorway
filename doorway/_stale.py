8291bb341e497bc91b807bb9267e1ef50794d44b
Nathan Michlo <nathanjmichlo@gmail.com>
2025-03-01 01:50:25 +0200

    upgrade licenses

--- doorway/_stale.py
+++ doorway/_stale.py
@@ -1,7 +1,7 @@
 #  ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
 #  MIT License
 #
-#  Copyright (c) 2021 Nathan Juraj Michlo
+#  Copyright (c) 2025 Nathan Juraj Michlo
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
